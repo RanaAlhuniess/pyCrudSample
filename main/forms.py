9d0f59e5575ce0c125c79bff6fbd44644282b2ed
ranahu <rana.al.huniess@l-one.de>
2022-12-29 20:56:54 +0300

    fix ui issue

--- main/forms.py
+++ main/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from .models import Customer
 
 class CustomerForm(forms.ModelForm):
+
     class Meta:
         model = Customer
         fields = ["first_name", "last_name", "email","birthday","grade"]
-        # widgets = {"birthday": forms.DateTimeField()}
+        widgets = {'birthday': forms.SelectDateWidget()}
