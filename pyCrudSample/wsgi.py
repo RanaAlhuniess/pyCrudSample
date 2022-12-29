a3e0ae925044502784e17638fa3fb70bf2891869
ranahu <rana.al.huniess@l-one.de>
2022-12-29 14:54:35 +0300

    create superadmin account when start the project

--- pyCrudSample/wsgi.py
+++ pyCrudSample/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth.models import User
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyCrudSample.settings')
 
 application = get_wsgi_application()
+
+if not User.objects.filter(username="admin").first():
+    User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
